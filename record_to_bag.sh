fce9cff9670171b8fde41e407ca2176f79da8571
johannesbyle <johannesbyle1@gmail.com>
2020-11-10 23:03:27 -0600

    renamed and increased wait time before starting bag in order to hopefully avoid some errors, this may be unnecessary

--- record_to_bag.sh
+++ record_to_bag.sh
@@ -1,30 +1,29 @@
 source devel/setup.bash
 
 # Start the connection to the imu (and start roscore in the background)
-roslaunch ros_mscl microstrain.launch&
+roslaunch ros_mscl microstrain.launch &
 sleep 5
 
 # Start the connection to the lidar
-rosrun velodyne_driver velodyne_node _model:=32C&
+rosrun velodyne_driver velodyne_node _model:=32C &
 sleep 5
 
 # Start conversion from lidar to pointcloud
-rosrun nodelet nodelet standalone velodyne_pointcloud/TransformNodelet&
-sleep 5
+rosrun nodelet nodelet standalone velodyne_pointcloud/TransformNodelet &
+sleep 10
 
 # Start recording specific nodes from the lidar and the imu
-rosbag record /gx5/imu/data /velodyne_points&
+rosbag record /gx5/imu/data /velodyne_points &
 sleep 5
 
 # Wait for user input and then attempt to kill gracefully
 exit_code="n"
-while [[ "$exit_code" != "y" ]]
-do
-	read -r -s -p "Stop recording (y/n): " exit_code
-	printf '\n'
+while [[ "$exit_code" != "y" ]]; do
+  read -r -s -p "Stop recording (y/n): " exit_code
+  printf '\n'
 done
 echo "$exit_code"
 echo "Exiting gracefully:"
-rosnode kill -a&
+rosnode kill -a &
 sleep 5
 kill -9
