0ebcb514a550a90eae81213c60648075ac3cd367
johannesbyle <johannesbyle1@gmail.com>
2020-11-18 10:10:52 -0600

    nodes now start at the same time

--- record_to_bag.sh
+++ record_to_bag.sh
@@ -1,21 +1,24 @@
-source devel/setup.bash
+source ~/catkin_ws/devel/setup.bash
+
 # Start recording specific nodes from the lidar and the imu
+roscore&
+sleep 5
+
 rosbag record /gx5/imu/data /velodyne_points &
+# rosbag record -a &
 sleep 5
 
 # Start the connection to the imu (and start roscore in the background)
 roslaunch ros_mscl microstrain.launch &
-sleep 5
 
 # Start the connection to the lidar
 rosrun velodyne_driver velodyne_node _model:=32C &
-sleep 5
 
 # Start conversion from lidar to pointcloud
 rosrun nodelet nodelet standalone velodyne_pointcloud/TransformNodelet _model:=32C _calibration:="$(rospack find velodyne_pointcloud)"/params/VeloView-VLP-32C.yaml&
-sleep 5
 
 # Wait for user input and then attempt to kill gracefully
+sleep 5
 exit_code="n"
 while [[ "$exit_code" != "y" ]]; do
   read -r -s -p "Stop recording (y/n): " exit_code
