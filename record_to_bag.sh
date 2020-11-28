122cc4a081157794fd9fef4cf558d45c99a40dca
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-11-28 20:07:30 +0100

    now looks for own custom launch file

--- record_to_bag.sh
+++ record_to_bag.sh
@@ -9,7 +9,7 @@ roslaunch ros_mscl microstrain.launch&
 
 # Start the connection to the lidar
 # rosrun velodyne_driver velodyne_node _model:=32C _npackets:=1 _rpm:=300&
-roslaunch velodyne_pointcloud VLP-32C_points.launch _npackets:=1 _rpm:=300&
+roslaunch VLP-32C_points.launch _npackets:=1 _rpm:=300&
 
 # Start conversion from lidar to pointcloud
 # rosrun nodelet nodelet standalone velodyne_pointcloud/TransformNodelet _model:=32C _calibration:="$(rospack find velodyne_pointcloud)"/params/VeloView-VLP-32C.yaml&
