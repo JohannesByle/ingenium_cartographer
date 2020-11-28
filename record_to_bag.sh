a2bda0f9a8a12d4d2226db22e5f999a4b5d45afa
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-11-28 19:34:45 +0100

    testing single launch file velodyne

--- record_to_bag.sh
+++ record_to_bag.sh
@@ -8,11 +8,12 @@ sleep 5
 roslaunch ros_mscl microstrain.launch &
 
 # Start the connection to the lidar
-rosrun velodyne_driver velodyne_node _model:=32C _npackets:=1 _rpm:=300&
+# rosrun velodyne_driver velodyne_node _model:=32C _npackets:=1 _rpm:=300&
+roslaunch velodyne_pointcloud VLP-32C_points.launch
 
 # Start conversion from lidar to pointcloud
-rosrun nodelet nodelet standalone velodyne_pointcloud/TransformNodelet _model:=32C _calibration:="$(rospack find velodyne_pointcloud)"/params/VeloView-VLP-32C.yaml&
-sleep 5
+# rosrun nodelet nodelet standalone velodyne_pointcloud/TransformNodelet _model:=32C _calibration:="$(rospack find velodyne_pointcloud)"/params/VeloView-VLP-32C.yaml&
+# sleep 5
 
 rosbag record /gx5/imu/data /velodyne_points&
 # rosbag record -a &
