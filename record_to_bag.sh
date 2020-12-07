24d7a219d31c64ce78b894253557cb168296d56e
johannesbyle <johannesbyle1@gmail.com>
2020-12-07 10:32:23 +0100

    testing moving point cloud conversion offline

--- record_to_bag.sh
+++ record_to_bag.sh
@@ -8,16 +8,11 @@ sleep 5
 roslaunch ros_mscl microstrain.launch&
 
 # Start the connection to the lidar
-# rosrun velodyne_driver velodyne_node _model:=32C _npackets:=1 _rpm:=300&
-roslaunch VLP-32C_points.launch _npackets:=1 _rpm:=300&
+rosrun velodyne_driver velodyne_node _model:=32C _npackets:=1 _rpm:=300&
+sleep 2
 
-# Start conversion from lidar to pointcloud
-# rosrun nodelet nodelet standalone velodyne_pointcloud/TransformNodelet _model:=32C _calibration:="$(rospack find velodyne_pointcloud)"/params/VeloView-VLP-32C.yaml&
-# sleep 5
-
-rosbag record /gx5/imu/data /velodyne_points&
-# rosbag record -a &
-sleep 5
+rosbag record /gx5/imu/data /velodyne_packets&
+sleep 2
 
 # Wait for user input and then attempt to kill gracefully
 sleep 5
