f7730e065e88223eb92d371ef94d2e7c03f02e37
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-12-09 19:00:25 +0100

    simplified, no longer need to run transform node

--- record_to_bag.sh
+++ record_to_bag.sh
@@ -1,21 +1,16 @@
-source ~/catkin_ws/devel/setup.bash
-
-# Start recording specific nodes from the lidar and the imu
-roscore&
-sleep 5
-
 # Start the connection to the imu (and start roscore in the background)
 roslaunch ros_mscl microstrain.launch&
+sleep 2
 
 # Start the connection to the lidar
 rosrun velodyne_driver velodyne_node _model:=32C _npackets:=1 _rpm:=300&
 sleep 2
 
+# Start recording specific nodes from the lidar and the imu
 rosbag record /gx5/imu/data /velodyne_packets&
 sleep 2
 
 # Wait for user input and then attempt to kill gracefully
-sleep 5
 exit_code="n"
 while [[ "$exit_code" != "y" ]]; do
   read -r -s -p "Stop recording (y/n): " exit_code
