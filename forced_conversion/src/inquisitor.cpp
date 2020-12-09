2d9d640f14ed95eb6d6e3e4c02d67f69da89d26d
johannesbyle <johannesbyle1@gmail.com>
2020-12-09 20:05:35 +0100

    inquisitor now works

--- forced_conversion/src/inquisitor.cpp
+++ forced_conversion/src/inquisitor.cpp
@@ -10,47 +10,44 @@
 #include <velodyne_pointcloud/rawdata.h>
 #include <velodyne_pointcloud/pointcloudXYZIRT.h>
 #include <velodyne_pointcloud/transform.h>
-#include <math.h>
+#include <cmath>
 
 #include <boost/foreach.hpp>
+
 #define foreach BOOST_FOREACH
 
 // Standard C++ entry point
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
+    std::string x(argv[1]);
+    std::string y(argv[2]);
     velodyne_rawdata::RawData data;
     double min_range = 0.0;
-    double max_range = 12000.0;
-    data.setParameters(min_range, max_range,0,2*M_PI);
-    data.setupOffline("/home/johannes/catkin_ws/src/velodyne/velodyne_pointcloud/params/VeloView-VLP-32C.yaml", max_range, min_range);
+    double max_range = 120.0;
+    data.setParameters(min_range, max_range, 0, 2 * M_PI);
+    data.setupOffline("/home/johannes/catkin_ws/src/velodyne/velodyne_pointcloud/params/VeloView-VLP-32C.yaml",
+                      max_range, min_range);
     rosbag::Bag new_bag;
-    new_bag.open("test.bag", rosbag::bagmode::Write);
+    new_bag.open(y, rosbag::bagmode::Write);
 
     rosbag::Bag bag;
-    bag.open("/home/johannes/Downloads/2020-11-25-09-49-03.bag", rosbag::bagmode::Read);
-    boost::shared_ptr<velodyne_rawdata::DataContainerBase> container_ptr;
+    bag.open(x, rosbag::bagmode::Read);
+    boost::shared_ptr <velodyne_rawdata::DataContainerBase> container_ptr;
     rosbag::View view(bag);
-    foreach(rosbag::MessageInstance const m, view)
+    foreach(rosbag::MessageInstance
+    const m, view)
     {
-        container_ptr = boost::shared_ptr<velodyne_pointcloud::PointcloudXYZIRT>(new velodyne_pointcloud::PointcloudXYZIRT(max_range, min_range, "velodyne", "odom", 1));
 
         velodyne_msgs::VelodyneScan::ConstPtr s = m.instantiate<velodyne_msgs::VelodyneScan>();
         if (s != NULL) {
-            std::cout << "\n";
-            std::cout << typeid(*container_ptr).name();
-            std::cout << "\n";
-            std::cout << typeid(container_ptr).name();
-            std::cout << "\n";
-            std::cout << "b3 setup \n";
-            container_ptr->setup(s);
-            std::cout << "after setup \n";
-            data.unpack(s->packets[0], *container_ptr, s->header.stamp);
-            std::cout << "after unpack \n";
-            sensor_msgs::PointCloud2 pcl = container_ptr->finishCloud();
-            std::cout << pcl;
-            new_bag.write("/velodyne_points", s->header.stamp, pcl);
+            sensor_msgs::PointCloud2Ptr pts = data.unpackOffline(s->packets[0], s->header.stamp);
+            new_bag.write("/velodyne_points", s->header.stamp, *pts);
+        }
+        sensor_msgs::Imu::ConstPtr r = m.instantiate<sensor_msgs::Imu>();
+        if (r != NULL) {
+            new_bag.write("/gx5/imu/data", r->header.stamp, *r);
         }
 
     }
-
+    std::cout << "completely done";
     bag.close();
 }
