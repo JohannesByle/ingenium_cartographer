0460c2a4a3abacaa8b2a80ccb408ac1c52c71795
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-12-20 15:38:56 +0100

    extra check unnecessary

--- process_bag_gui.sh
+++ process_bag_gui.sh
@@ -1,8 +1,4 @@
 cd ~/ingenium_cartographer/ || exit
 file=$(zenity --file-selection --title="Choose a bag file" --file-filter="*.bag")
-if [ ! -f "$file" ]; then
-  echo "$file is not a file."
-  return 1
-fi
 chmod +x ~/ingenium_cartographer/process_bag.sh
 gnome-terminal --working-directory=/home/johannes/ingenium_cartographer/ -- ~/ingenium_cartographer/process_bag.sh "$file"
