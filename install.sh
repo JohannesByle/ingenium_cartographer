41eb6fa540d353d9bb6598b457470c0f5e55a136
johannesbyle <johannesbyle1@gmail.com>
2020-12-21 19:53:00 +0100

    still not working on local machine

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cwd=$(pwd)
-cd ~/catkin_ws_test/src || exit
+cd ~/catkin_ws/src || exit
 
 git clone https://github.com/ros-drivers/velodyne
 cd velodyne || exit
@@ -9,19 +9,21 @@ git pull
 cd .. || exit
 
 git clone https://github.com/LORD-MicroStrain/ROS-MSCL
-cd ROS-MSCL || exit
-# This stage of the repo was tested to be working, but may not work with future ROS releases
-git reset --hard ecf15a7
-git pull
-cd .. || exit
+#cd ROS-MSCL || exit
+## This stage of the repo was tested to be working, but may not work with future ROS releases
+#git reset --hard ecf15a7
+#git pull
+#cd .. || exit
+#mv ROS-MSCL/ros_mscl .
+#sudo rm -r ROS-MSCL
 
-cd ~/catkin_ws_test/ || exit
+cd ~/catkin_ws/ || exit
 catkin_make_isolated --install --use-ninja
-cd ~/catkin_ws_test/src || exit
+cd ~/catkin_ws/src || exit
 
 sudo rm -r velodyne
 git clone https://github.com/JohannesByle/velodyne
 
-cd ~/catkin_ws_test/ || exit
+cd ~/catkin_ws/ || exit
 catkin_make_isolated --install --use-ninja
 cd "$cwd" || exit
