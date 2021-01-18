0852067d47b0ebfa722c696c4a4702fc76575734
johannesbyle <johannesbyle1@gmail.com>
2021-01-18 18:30:57 +0100

    removed all hardcoded references to home directory

--- display_bag_gui.sh
+++ display_bag_gui.sh
@@ -2,4 +2,4 @@ source install_isolated/setup.bash
 cd ~/ingenium_cartographer/ || exit
 file=$(zenity --file-selection --title="Choose a bag file" --file-filter="*.bag")
 chmod +x ~/ingenium_cartographer/display_bag.sh
-gnome-terminal --working-directory=/home/johannes/ingenium_cartographer/ -- ~/ingenium_cartographer/display_bag.sh "$file"
+gnome-terminal --working-directory=~/ingenium_cartographer/ -- ~/ingenium_cartographer/display_bag.sh "$file"
--- process_bag_gui.sh
+++ process_bag_gui.sh
@@ -1,4 +1,4 @@
 cd ~/ingenium_cartographer/ || exit
 file=$(zenity --file-selection --title="Choose a bag file" --file-filter="*.bag")
 chmod +x ~/ingenium_cartographer/process_bag.sh
-gnome-terminal --working-directory=/home/johannes/ingenium_cartographer/ -- ~/ingenium_cartographer/process_bag.sh "$file"
+gnome-terminal --working-directory=~/ingenium_cartographer/ -- ~/ingenium_cartographer/process_bag.sh "$file"
